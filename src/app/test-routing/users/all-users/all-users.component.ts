e44712f93e63b673fdfe910bf01ad5c72a0b8534
Your Name <mangelov@iqsoft.bg>
2021-01-26 10:19:36 +0200

    pagination work

--- src/app/test-routing/users/all-users/all-users.component.ts
+++ src/app/test-routing/users/all-users/all-users.component.ts
@@ -12,18 +12,19 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class AllUsersComponent implements OnInit {
   displayedColumns: string[] = ['userId', 'username', 'name', 'email', 'userAddressCity', 'userAddressStreet', 'userAddressSuite', 'userAddressZipcode'];
-  pageSize: number[] = [5, 10, 15];
+  pageSize: number[] = [2, 5, 8];
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
   users: Array<IUser>
-  dataSource;
+  dataSource = new MatTableDataSource<IUser>();
   constructor(private usersService: UsersService) { }
 
   ngOnInit(): void {
     this.usersService.getAllUsers().subscribe((data) => {
       this.users = data;
       this.dataSource = new MatTableDataSource<IUser>(this.users);
-
+      this.dataSource.paginator = this.paginator;
     })
   }
 }
